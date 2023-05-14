b5892536efcb88b0b812d658431824a115ef4816
iYEiD <202110931@ua.edu.lb>
2023-05-14 16:30:23 +0300

    pagination

--- src/main/java/bookstore/api/v1/Controller/BookController.java
+++ src/main/java/bookstore/api/v1/Controller/BookController.java
@@ -3,6 +3,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import bookstore.api.v1.Service.BookService;
 import bookstore.api.v1.Model.Book;
@@ -54,5 +59,19 @@ public class BookController {
     @DeleteMapping("/deleteAll")
     public void deleteAllBooks() {
         bookService.deleteAllBooks();
+    }
+
+    @GetMapping("/paged/{page}/{size}/{sortField}/{sortOrder}")
+    public ResponseEntity<List<Book>> getAllBooks(@PathVariable int page,@PathVariable int size,@PathVariable String sortField, @PathVariable String sortOrder) {
+
+        Page<Book> bookPage = bookService.getAllBooks(page, size, sortField, sortOrder);
+
+        List<Book> books = bookPage.getContent();
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("X-Total-Count", String.valueOf(bookPage.getTotalElements()));
+        responseHeaders.set("X-Page-Number", String.valueOf(bookPage.getNumber()));
+        responseHeaders.set("X-Page-Size", String.valueOf(bookPage.getSize()));
+
+        return new ResponseEntity<>(books, responseHeaders, HttpStatus.OK);
     }
 }
--- src/main/java/bookstore/api/v1/Service/BookService.java
+++ src/main/java/bookstore/api/v1/Service/BookService.java
@@ -3,6 +3,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -24,6 +28,21 @@ public List<Book> getAllBooks() {
     return bookRepository.findAll();
 
 }
+//for pagination
+public Page<Book> getAllBooks(int page, int size, String sortField, String sortOrder) {
+    Sort.Direction direction = Sort.Direction.ASC;
+    if (sortOrder.equalsIgnoreCase("desc")) {
+        direction = Sort.Direction.DESC;
+    }
+    
+    Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortField));
+    return bookRepository.findAll(pageable);
+}
+
+
+
+
+
 
 public Optional<Book> getBookById(String id) {
     Optional<Book> bookOptional = bookRepository.findById(id);
@@ -67,5 +86,8 @@ public void deleteAllBooks() {
     bookRepository.deleteAll();
 }
 
-}
 
+
+
+
+}
